79d7e487d516d80325fd0f1f9d87532129401ad0
Gabriel2392 <gabriel824m@gmail.com>
2022-11-14 17:13:07 +0000

    a10: adapt to ekrp

--- twrp_a10.mk
+++ twrp_a10.mk
@@ -18,7 +18,7 @@
 PRODUCT_RELEASE_NAME := a10
 
 # Inherit some common stuff.
-$(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, vendor/eureka/config/common.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
